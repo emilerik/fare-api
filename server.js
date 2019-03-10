de7b3bab9ce31e5a5d7a2078c7ddcdbded64cb31
emilerik <emil.eriksson41@gmail.com>
2019-03-10 23:14:05 +0100

    partioned server.js into severasub files

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+//Controllers
+const register = require('./controllers/register.js')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+//Establishing database connection
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'Emil',
+    password: '',
+    database: 'faredb'
+  }
+});
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//End-points
+app.get('/', (req, res) => {res.send('this is a server')});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.put('/image', image.handleImage(db));
+
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
+});
+
